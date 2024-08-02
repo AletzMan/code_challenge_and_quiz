f1e21de3adb299c8db6c715656b49a7cda6dd13b
= <=>
2024-08-01 18:58:49 -0600

    Chore: create main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,42 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link"
+import { Button } from "./components/Button/Button"
+import { FlowChartIcon, QuestionIcon, SkillIcon, StarIcon } from "./components/Icons"
+import styles from "./page.module.css"
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<main className={styles.main}>
+			<div className={styles.code}>
+				<div className={styles.code_image}></div>
+				<div className={styles.shadow}></div>
+			</div>
+			<h2 className={styles.title}>¡Desafía tu mente, domina el código!</h2>
+			<article className={styles.article}>
+				<p className={styles.message}>Mejora y practica tus habilidades de programación con cuestionarios interactivos y retos de algoritmos.</p>
+				<ul className={styles.ul}>
+					<li className={styles.li}>
+						<h3>Quizzes Interactivos</h3>
+						<StarIcon className={styles.li_icon} />
+						Pon a prueba tu conocimiento con preguntas sobre diversos lenguajes de programación. Perfecto para practicar y prepararte para entrevistas técnicas.
+						<QuestionIcon className={`${styles.li_image} ${styles.li_imageOne}`} />
+					</li>
+					<li className={styles.li}>
+						<h3>Desafíos de Algoritmos</h3>
+						<StarIcon className={styles.li_icon} />
+						Enfrenta desafíos de código y recibe retroalimentación instantánea de nuestra IA
+						<FlowChartIcon className={`${styles.li_image} ${styles.li_imageTwo}`} />
+					</li>
+					<li className={styles.li}>
+						<h3>Corrección Automática</h3>
+						<StarIcon className={styles.li_icon} />
+						Mejora tus habilidades con explicaciones detalladas y soluciones paso a paso
+						<SkillIcon className={`${styles.li_image} ${styles.li_imageThree}`} />
+					</li>
+				</ul>
+			</article>
+			<Link className={styles.button} href="/playground" >
+				¡EMPIEZA AHORA!
+			</Link>
+		</main>
+	)
 }
