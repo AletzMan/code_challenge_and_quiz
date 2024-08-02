a1bc6e133db808a175ad437248a78d74226137f7
= <=>
2024-08-01 19:01:00 -0600

    chore: create quiz elements

---
+++ app/(pages)/playground/quiz/components/PlaygroundQuiz/PlaygroundQuiz.tsx
@@ -0,0 +1,36 @@
+"use client"
+import styles from "./styles.module.scss"
+import { QuizSetup } from "../QuizSetup/QuizSetup"
+import { useState } from "react"
+import { Questions } from "../Questions/Questions"
+
+export interface IItemCategory {
+    option: string,
+    value: string,
+    functionSyntax: string,
+    color: string
+}
+
+export const PlaygroundQuiz = () => {
+
+    const [start, setStart] = useState(false)
+
+    const HandleStart = () => {
+        setStart(true)
+    }
+
+    return (
+        <section className={styles.section}>
+            {!start && <>
+                <QuizSetup />
+                <button className={styles.start} onClick={HandleStart}>
+                    COMENZAR
+                </button>
+            </>}
+            {start &&
+                <Questions setStart={setStart} />
+            }
+        </section>
+    )
+}
+
