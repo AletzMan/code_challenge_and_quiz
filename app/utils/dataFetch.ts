153e82f678967891188643a04b8a4c842f270fe1
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-08-06 05:18:02 -0600

    CHore: add URL api

--- app/utils/dataFetch.ts
+++ app/utils/dataFetch.ts
@@ -1,8 +1,10 @@
 "use server"
 
+const URL = process.env.URL_API
+
 export const GetNewQuiz = async (language: string, level: string) => {
     const type = (Math.random() * 10).toFixed(0)
-    const response = await fetch(`http://localhost:3000/api/quiz?language=${language}&level=${level}&type=${type}`, { next: { revalidate: 10 } })
+    const response = await fetch(`${URL}/api/quiz?language=${language}&level=${level}&type=${type}`, { next: { revalidate: 10 } })
     const data = await response.json()
     if (response.ok) {
         return JSON.parse(data.data)
