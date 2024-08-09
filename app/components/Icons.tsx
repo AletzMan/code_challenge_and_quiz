51552ad341f1c1599724ffadb9ac6b793376c200
= <=>
2024-08-09 01:26:19 -0600

    Chore: add icon add

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -457,3 +457,7 @@ export const CopyIcon = (props: SVGProps<SVGSVGElement>) => (
 export const RunCodeIcon = (props: SVGProps<SVGSVGElement>) => (
     <svg {...props} width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M20 19V7H4v12zm0-16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-7 14v-2h5v2zm-3.42-4L5.57 9H8.4l3.3 3.3c.39.39.39 1.03 0 1.42L8.42 17H5.59z"></path></svg>
 )
+
+export const AddIcon = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...props} width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M7.007 12a.75.75 0 0 1 .75-.75h3.493V7.757a.75.75 0 0 1 1.5 0v3.493h3.493a.75.75 0 1 1 0 1.5H12.75v3.493a.75.75 0 0 1-1.5 0V12.75H7.757a.75.75 0 0 1-.75-.75"></path><path fill="currentColor" fillRule="evenodd" d="M7.317 3.769a42.5 42.5 0 0 1 9.366 0c1.827.204 3.302 1.643 3.516 3.48c.37 3.157.37 6.346 0 9.503c-.215 1.837-1.69 3.275-3.516 3.48a42.5 42.5 0 0 1-9.366 0c-1.827-.205-3.302-1.643-3.516-3.48a41 41 0 0 1 0-9.503c.214-1.837 1.69-3.276 3.516-3.48m9.2 1.49a41 41 0 0 0-9.034 0A2.486 2.486 0 0 0 5.29 7.424a39.4 39.4 0 0 0 0 9.154a2.486 2.486 0 0 0 2.193 2.164c2.977.332 6.057.332 9.034 0a2.486 2.486 0 0 0 2.192-2.164a39.4 39.4 0 0 0 0-9.154a2.486 2.486 0 0 0-2.192-2.163" clipRule="evenodd"></path></svg>
+)
