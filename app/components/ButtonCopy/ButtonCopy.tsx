823bc27643f625c295a8077922e8f9f1cd3c6d41
= <=>
2024-08-09 01:21:43 -0600

    Update: styles and icons

--- app/components/ButtonCopy/ButtonCopy.tsx
+++ app/components/ButtonCopy/ButtonCopy.tsx
@@ -16,14 +16,15 @@ export function ButtonCopy({ textToCopy }: Props) {
 
         setTimeout(() => {
             setCopy(false)
-        }, 1000)
+        }, 1500)
     }
 
     return (
         <button
             className={`${styles.button} ${copy && styles.button_check} `} onMouseDown={HandleCopyClipBoard}>
-            {!copy && <CopyIcon className={styles.button_close} />}
-            {copy && <CheckIcon className={styles.button_close} />}
+            {!copy && <CopyIcon className={styles.button_icon} />}
+            {copy && <CheckIcon className={`${styles.button_icon}  `} />}
+            {copy && <span className={`  ${styles.button_iconCheck}`} >¡Copiado!</span>}
         </button>
     )
 }
