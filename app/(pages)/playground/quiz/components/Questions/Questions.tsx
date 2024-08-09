5be878e79c1bd76e5376e3994372999078d39965
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-08-09 12:32:35 -0600

    Refactor: extract results component to create a separate one

--- app/(pages)/playground/quiz/components/Questions/Questions.tsx
+++ app/(pages)/playground/quiz/components/Questions/Questions.tsx
@@ -17,6 +17,7 @@ import { useSnackbar } from "notistack"
 import { Modal } from "@/app/components/Modal/Modal"
 import { CATEGORIES, StyleCodeEditor } from "@/app/utils/const"
 import { ButtonClose } from "@/app/components/ButtonClose/ButtonClose"
+import { Results } from "../Results/Results"
 
 interface Props {
     setStart: Dispatch<SetStateAction<boolean>>
@@ -257,37 +258,7 @@ export function Questions({ setStart }: Props) {
                     }
                     {
                         viewResults &&
-                        <article className={`${styles.results}`}>
-                            <h4 className={styles.results_title}>Resultados</h4>
-                            <div className={styles.results_subtitle}>
-                                <span className={styles.results_label}>Preguntas: <span className={styles.results_number}>{completeQuiz.questions.length}</span></span>
-                                <span className={styles.results_label}>Aciertos: <span className={styles.results_number}>{completeQuiz.correctAnswers}</span></span>
-                            </div>
-                            <ol className={styles.question}>
-                                {completeQuiz.questions.map(question => (
-                                    <li key={question.question} className={styles.question_text}>
-                                        <div className={styles.question_textLi}>
-                                            {question.question}
-                                            <span className={`${styles.question_answer} ${question.isRight && styles.question_answerRight}`}>{question.answer}</span>
-                                            <details className={styles.details} name="explication">
-                                                <summary className={styles.details_summary}>
-                                                    <span className={styles.details_title} > Explicación<ArrowUpIcon className={styles.details_arrow} /> </span>
-                                                </summary>
-                                                <p className={styles.details_p}>{question.explanation}</p>
-                                                {question.codeSnippetExplanation &&
-                                                    <CodeBlock text={question.codeSnippetExplanation.replaceAll('\\n', '\n').replaceAll('\\t', '\t').replaceAll('\\', '')}
-                                                        theme={atomOneDark} language={language.language}
-                                                        customStyle={{ ...StyleCodeEditor }}
-                                                    />
-                                                }
-                                            </details>
-                                        </div>
-                                    </li>
-                                ))
-                                }
-                            </ol>
-
-                        </article>
+                        <Results />
                     }
                     {(!loading && !error) &&
                         <Loading title="Generando pregunta..." />
