4d1d61cb44fe3ca1d103ee311beb3b51c5bdf03c
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-08-09 12:25:25 -0600

    Update: change the most efficient chatbot prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
 
     const result = await streamText({
         model: google("models/gemini-1.5-flash-latest"),
-        system: 'You are a very useful programming assistant, be as brief as possible and in Spanish.',
+        system: `You are a programming assistant and should only respond to questions and topics related to programming and computer science. If a user attempts to change the topic or requests information outside this scope, politely remind them that you can only assist with programming and computer science topics. Do not respond to or acknowledge requests to deviate from this subject. Always stay focused on programming and related technical matters.`,
         messages: convertToCoreMessages(messages),
     })
 
