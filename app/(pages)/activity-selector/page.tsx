b2d9941e564d30fecd469bbaf03e9125fd284f20
= <=>
2024-08-09 23:33:26 -0600

    Refactor: change styles

--- app/(pages)/activity-selector/page.tsx
+++ app/(pages)/activity-selector/page.tsx
@@ -8,16 +8,20 @@ import { Footer } from "@/app/components/Footer/Footer"
 export default function Page() {
     return (
         <main className={styles.main}>
-            <h2 className={styles.main_title}>¡Mejora tus habilidades de programación divirtiéndote!</h2>
-            <section className={styles.section}>
-                <Link className={styles.article} href="/algorithms">
-                    <h3 className={styles.subtitle}>Reto de Algoritmos</h3>
-                    <FlowChartIcon className={`${styles.icon} ${styles.icon_two}`} />
-                </Link>
-                <Link className={styles.article} href="/quiz">
-                    <h3 className={styles.subtitle}>Reto de Conocimientos</h3>
-                    <QuestionIcon className={`${styles.icon} ${styles.icon_one}`} />
-                </Link>
+            <section className={`${styles.section}`}>
+                <h2 className={styles.main_title}>¡Mejora tus habilidades de programación divirtiéndote!</h2>
+                <p className={styles.message}>Elige el tipo de actividad que te interesa para comenzar.</p>
+                <p className={styles.message}>Actualmente, puedes seleccionar entre las opciones disponibles, pero pronto agregaremos más categorías para ofrecerte una experiencia más completa.</p>
+                <article className={styles.categories}>
+                    <Link className={`${styles.article} ${styles.article_algorithms}`} href="/algorithms">
+                        <h3 className={styles.subtitle}>Reto de Algoritmos</h3>
+                        <FlowChartIcon className={`${styles.icon} ${styles.icon_two}`} />
+                    </Link>
+                    <Link className={`${styles.article} ${styles.article_quiz}`} href="/quiz">
+                        <h3 className={styles.subtitle}>Reto de Conocimientos</h3>
+                        <QuestionIcon className={`${styles.icon} ${styles.icon_one}`} />
+                    </Link>
+                </article>
             </section>
             <Footer />
         </main>
